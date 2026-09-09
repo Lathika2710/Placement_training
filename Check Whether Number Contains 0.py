dd56587a7165bf5f6ecaf16b13bb945837d105b9
lathikap25it-collab <lathika.p_25it@kgkite.ac.in>
2026-09-09 10:14:40 +0530

    no contain 0

---
+++ Check Whether Number Contains 0.py
@@ -0,0 +1,14 @@
+n = int(input())
+found = False
+
+while n > 0:
+    if n % 10 == 0:
+        found = True
+        break
+
+    n //= 10
+
+if found:
+    print("Contains 0")
+else:
+    print("Does not contain 0")
